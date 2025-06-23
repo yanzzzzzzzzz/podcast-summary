1bceecb4b2c46145714d333f832c7d10aca659c4
Jack Tsai <jtsai@nexify.com.hk>
2025-06-23 16:57:11 +0800

    port setting by .env

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.use(express.json());
 app.use('/api', uploadRouter);
