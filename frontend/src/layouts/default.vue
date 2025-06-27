709881437fe514ea61ee09dfb897ad3a89b8285a
Jack Tsai <jtsai@nexify.com.hk>
2025-06-27 16:15:10 +0800

    Hidden footer

--- frontend/src/layouts/default.vue
+++ frontend/src/layouts/default.vue
@@ -4,11 +4,11 @@
     <v-main>
       <router-view />
     </v-main>
-    <AppFooter />
+    <!-- <AppFooter /> -->
   </v-app>
 </template>
 
 <script setup lang="ts">
 import AppNavBar from '@/components/AppNavBar.vue';
-import AppFooter from '@/components/AppFooter.vue';
+// import AppFooter from '@/components/AppFooter.vue';
 </script>
